40f473851cbe68dc53762b21852aaa6b6a03ad75
HSSJW <okqkfkfk@naver.com>
2025-12-04 14:31:13 +0900

    Initial commit: myyak-server Spring Boot project

---
+++ src/main/java/com/myyak/MyyakServerApplication.java
@@ -0,0 +1,13 @@
+package com.myyak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyyakServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyyakServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/myyak/MyyakServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myyak;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyyakServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
