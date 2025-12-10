a92aac3795cbb05732118e62c15b6fa9bad69d78
HSSJW <okqkfkfk@naver.com>
2025-12-10 14:31:53 +0900

    [#12] Feat : UserMedication에 prescriptionId 필드 추가

--- src/main/java/com/myyak/repository/UserMedicationRepository.java
+++ src/main/java/com/myyak/repository/UserMedicationRepository.java
@@ -35,4 +35,12 @@ public interface UserMedicationRepository extends JpaRepository<UserMedication, 
     // 특정 DrugInfo를 사용하는 사용자의 약물
     @Query("SELECT um FROM UserMedication um WHERE um.user = :user AND um.drugInfo.itemSeq = :itemSeq AND um.isActive = true")
     Optional<UserMedication> findByUserAndDrugItemSeq(@Param("user") User user, @Param("itemSeq") String itemSeq);
+
+    // 처방전 ID로 약물 목록 조회
+    @Query("SELECT um FROM UserMedication um LEFT JOIN FETCH um.drugInfo WHERE um.prescriptionId = :prescriptionId")
+    List<UserMedication> findByPrescriptionId(@Param("prescriptionId") Long prescriptionId);
+
+    // 처방전 ID로 약물 개수 조회
+    @Query("SELECT COUNT(um) FROM UserMedication um WHERE um.prescriptionId = :prescriptionId")
+    int countByPrescriptionId(@Param("prescriptionId") Long prescriptionId);
 }
